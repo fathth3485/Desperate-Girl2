5affd028464e1b403d54fe21960d0a97ef5aa647
fathth3485 <45114815+fathth3485@users.noreply.github.com>
2018-12-14 17:50:17 -0500

    Add files via upload

---
+++ EnemyGun.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGun : MonoBehaviour
+{
+    public GameObject EnemyBulletGo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("FireEnemyBullet", 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FireEnemyBullet()
+    {
+        GameObject playerShip = GameObject.Find("Girl");
+
+        if(playerShip != null)
+        {
+            GameObject bullet = (GameObject)Instantiate(EnemyBulletGo);
+            bullet.transform.position = transform.position;
+            Vector2 direction = playerShip.transform.position - bullet.transform.position;
+            bullet.GetComponent<EnemyBullet>().SetDirection(direction);
+        }
+    }
+}
---
+++ EnemySpawner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject Enemy;
+    float maxSpawnRateInSeconds = 2;
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("SpawnEnemy", maxSpawnRateInSeconds);
+
+        InvokeRepeating("IncreaseSpawnRate", 0, 30);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SpawnEnemy()
+    {
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        GameObject anEnemy = (GameObject)Instantiate(Enemy);
+        anEnemy.transform.position = new Vector2(Random.Range(min.x, max.x), max.y);
+
+        ScheduleNextEnemySpawn();
+
+        void ScheduleNextEnemySpawn()
+        {
+            float spawnInSeconds;
+            if (maxSpawnRateInSeconds > 1)
+            {
+                spawnInSeconds = Random.Range(1, maxSpawnRateInSeconds);
+            }
+            else
+                spawnInSeconds = 1;
+            Invoke("SpawnEnemy", spawnInSeconds);
+        }
+        void IncreaseSpawnRate()
+        {
+            if (maxSpawnRateInSeconds > 1)
+                maxSpawnRateInSeconds--;
+
+            if (maxSpawnRateInSeconds == 1)
+                CancelInvoke("IncreaseSpawnRate");
+
+        }
+
+
+
+    }
+}
